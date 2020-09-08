fc6055662914ab8655a811be00cd985f3408bad0
Sam Markham <sam.e.markham@gmail.com>
2020-09-08 16:34:57 -0700

    Adds prompt generator code into home index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,11 @@
 import { h } from 'preact';
 import style from './style.css';
+import Generator from '../components/generator';
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
+		<p>Write 250 words about:</p>
+    <Generator dataset={words}/>
 	</div>
 );
 

e02e328c37c718b13d13b884c279a0d0ccd7f593
Sam Markham <sam.e.markham@gmail.com>
2020-09-08 17:09:02 -0700

    Adds content in About

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -3,7 +3,8 @@ import style from './style.css';
 
 const About = () => (
 	<div class={style.about}>
-		<p>Write 250 words about:</p>
+      <p>This is a writing prompt generator built with Preact. Check out the code on GitHub <a target="blank" href="https://github.com/sharkham/preact-writing-prompt-generator">here</a>!</p>
+      <p>Want to get in touch? You can find my contact info <a target="blank" href="https://github.com/sharkham">here</a>.</p>
 	</div>
 );
 

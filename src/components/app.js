296909292ac51719b47cbc346ea08dbae29449f5
Sam Markham <sam.e.markham@gmail.com>
2020-09-08 17:04:03 -0700

    Adds nautical route to app

--- src/components/app.js
+++ src/components/app.js
@@ -5,12 +5,14 @@ import Header from './header';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
+import Nautical from '../routes/nautical'
 
 const App = () => (
 	<div id="app">
 		<Header />
 		<Router>
 			<Home path="/" />
+      <Nautical path="/nautical"/>
 		</Router>
 	</div>
 )

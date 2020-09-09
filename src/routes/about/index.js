b0a059dc1414c5d3c397e912f0b9d94d8c2b3464
Sam Markham <sam.e.markham@gmail.com>
2020-09-08 17:07:17 -0700

    Adds about component (index and style.css)

---
+++ src/routes/about/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+import style from './style.css';
+
+const About = () => (
+	<div class={style.about}>
+		<p>Write 250 words about:</p>
+	</div>
+);
+
+export default About;
